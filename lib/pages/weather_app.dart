bab9f2c676ce4ba9add874f942ac11fc98628814
Tawasoul <tawasoulmohamedkhair@gmail.com>
2024-02-20 07:49:57 -0700

    The first page of weather app

---
+++ lib/pages/weather_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+  @override
+  State<WeatherPage> createState() => _WeatherPage();
+}
+
+class _WeatherPage extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
