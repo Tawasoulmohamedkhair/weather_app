6cf3578e9beb67f3a44cbadde4887ceb6c9c3044
Tawasoul <tawasoulmohamedkhair@gmail.com>
2024-02-21 02:54:16 -0700

    The  weather page

--- lib/pages/weather_app.dart
+++ lib/pages/weather_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weathr_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -7,8 +9,43 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPage extends State<WeatherPage> {
+  //apikey
+  final _weatherService = WeatherService('efd3321346f2b9acbebc716e03a47fe4');
+  Weather? _weather;
+  //fetch weather
+  _fetchWeather() async {
+    //get the current city
+    String cityName = await _weatherService.getCurrenCity();
+    //get weather for  city
+    try {
+      final Weather = await _weatherService.getweather(cityName);
+      setState(() {
+        _weather = Weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //cityname
+            Text(_weather?.cityName ?? 'Loading city ..'),
+            //temperature
+            Text('${_weather?.temperature.round()}  ْ C'),
+          ],
+        ),
+      ),
+    );
   }
 }
